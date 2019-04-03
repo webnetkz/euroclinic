aa675ff1ef20f89e44f4f947f2a8856b2546b92f
webnetkz <8gg001@gmail.com>
2019-04-03 20:11:58 +0700

    update style

--- index.php
+++ index.php
@@ -8,17 +8,13 @@
 <!DOCTYPE html>
 <html lang="ru">
     <head>
-        <title>Emirmed</title>
+        <title>Euroclinic</title>
 
         <meta charset="UTF-8">
         <meta name="theme-color" content="rgb(255, 255, 255)">
         <meta name="author" content="Emirmed">
-        <meta name="description" content="Медицинский центр Emirmed, режим работы круглосуточно без выходных и праздников.">
-        <meta name="keywords" content="УЗИ круглосуточно,
-        уролог круглосуточно,
-        гинеколог круглосуточно,
-        анализы круглосуточно,
-        ЛОР круглосуточно">
+        <meta name="description" content="Медицинский центр Emirmed, режим работы круглосуточно без выходных и праздников. Вывод из запоя">
+        <meta name="keywords" content="Вывод из запоя круглосуточно, медицинский центр круглосуточно, круглосуточный таксиколог, круглосуточный нарколог">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="index, follow">
 
@@ -34,123 +30,36 @@
         <div id="all">
 
             <header>
-                
-                <ul class="menuHeader">
-                    <li>
-                        <a href="#" class="menuLink">
-                            Главная
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/uslugi.php" class="menuLink">
-                            Услуги
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/price.php" class="menuLink">
-                            Цены
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/doctors.php" class="menuLink">
-                            Врачи
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/clinik.php" class="menuLink">
-                            Клиника
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/news.php" class="menuLink">
-                            Новости
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pages/contact.php" class="menuLink">
-                            Контакты
-                        </a>
-                    </li>
-                </ul>
-
-                <p class="numberPhone">+7 707 555-05-78 / +7 727 354-46-86</p>    
-
-                <div class="blockLogo">
-                    <a href="#">
-                        <img src="/public/img/logoEmirmed.png" alt="logotype for Emirmed" class="logotype">
-                    </a>
+            
+                <div class="one">
+                    <img src="/public/img/24.svg" alt="icons 24 hours">
+                    <p class="textS">КРУГЛОСУТОЧНО 24/7</p>
+                </div>
+                <a href="#">
+                    <div class="two">
+                        <img src="/public/img/logoEmirmed.png" alt="Logotype emirmed">
+                    </div>
+                 </a>
+                <div class="three">
+                    <p class="callBack">заказать звонок</p>
+                    <p>
+                        +7 707 555 05 78
+                    </p>
+                    <p>
+                        +7 727 354 46 86
+                    </p>
                 </div>
 
             </header>
 
             <div id="content">
-                <div class="decoration1"></div>
-                <div class="decoration2"></div>
-                <div class="decoration3"></div>
-                    <div class="decoration4"></div>
-                    <div class="decoration5"></div>
-                    <div class="decoration6"></div>
-                    <div class="decoration7"></div>
-                    <div class="decoration8"></div>
+
             </div>
 
             <footer>
 
-                    <div id="footerStart">
-                            <h3 class="contactHeader">Навигация по сайту:</h3>
-                        <hr class="footer">
-                        <ul class="menuFooter">
-                            <li>
-                                <a href="#" class="footerMenuItem">
-                                    Главная
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/uslugi.php" class="footerMenuItem">
-                                    Услуги
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/price.php" class="footerMenuItem">
-                                    Цены
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/doctors.php" class="footerMenuItem">
-                                    Врачи
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/clinik.php" class="footerMenuItem">
-                                    Клиника
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/news.php" class="footerMenuItem">
-                                    Новости
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/contact.php" class="footerMenuItem">
-                                    Контакты
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
 
-                <a href="#">
-                    <img src="/public/img/logoEmirmed.png" alt="logotype emirmed" class="footerLogotype">
-                </a>
-                <p class="date">
-                    © 2019 Все права защищены.
-                    <a href="#" class="indexLink">Emirmed</a>
-                </p>
-
-                    <div id="footerEnd">
-                            <h3 class="contactHeader">Контакты:</h3>
-                        <hr class="footer">
-                    </div>
-
+                   
             </footer>
 
         </div> <!-- END DIV #ALL-->
@@ -164,10 +73,6 @@
             <input type="text" autocomplete="off" class="inpPhone" placeholder="номер телефона" name="phoneInp">
             <input type="submit" value="заказать звонок" class="inpPhone phoneBtn" name="btnInp">
         </form>
-
-        <div class="workTime">
-            <p>Круглосуточно 24/7</p>
-        </div>
 
         <script>
              // Проверка на поддержку service worker

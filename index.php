e496fc5671cd81147b75b0474d2f7c541acb835c
webnetkz <8gg001@gmail.com>
2019-04-04 17:57:13 +0700

    fix small bugs

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ mail($to, $subject, $message, $headers);
 
             <footer>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1728.3879109509546!2d76.90879461367192!3d43.23485405832976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skz!4v1554352800652!5m2!1sru!2skz" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe title="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1728.3879109509546!2d76.90879461367192!3d43.23485405832976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skz!4v1554352800652!5m2!1sru!2skz" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                    
             </footer>
 
